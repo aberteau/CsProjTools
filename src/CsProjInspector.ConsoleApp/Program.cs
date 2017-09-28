dd72b0ec4536e8ec6fbba8077290deb68442000d
Amael BERTEAU <amael.berteau@tech-easy.fr>
2017-09-28 17:37:54 +0200

    Prise en charge des command line arguments

--- src/CsProjInspector.ConsoleApp/Program.cs
+++ src/CsProjInspector.ConsoleApp/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using CsProjTools.CsProjInspector.Data;
+using System.Reflection;
 using CsProjTools.CsProjInspector.DocGenerator;
+using NDesk.Options;
 
 namespace CsProjTools.CsProjInspector.ConsoleApp
 {
@@ -10,13 +10,61 @@ namespace CsProjTools.CsProjInspector.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string path = @"D:\Temp\OutputPaths";
+            bool helpFlag = false;
+            String csprojPath = null;
+            String outputPath = null;
 
-            string chrono = FilenameHelper.GetChrono();
-            string xlsFilename = $"{chrono} - CsProjInspector Output.xlsx";
-            string xlsFilePath = Path.Combine(@"D:\Temp\", xlsFilename);
+            OptionSet optionSet = new OptionSet() {
+                { "csprojPath=", "the {PATH} of csproj folder. [Mandatory]", v => csprojPath = v },
+                { "outputPath=", "the {PATH} of output file.", v => outputPath = v },
+                { "h|help",  "show this message and exit", v => helpFlag = v != null },
+            };
 
-            ExcelHelper.GenerateExcelFile(path, xlsFilePath);
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            string applicationName = Path.GetFileName(codeBase);
+
+            try
+            {
+                optionSet.Parse(args);
+
+                if (String.IsNullOrWhiteSpace(csprojPath))
+                    throw new Exception("csprojPath must be provided");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Use '{applicationName} --help' for more information.");
+                return;
+            }
+
+            if (helpFlag)
+            {
+                ShowHelp(optionSet);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(outputPath))
+            {
+                string applicationFolder = Path.GetDirectoryName(codeBase);
+                string chrono = FilenameHelper.GetChrono();
+                string xlsFilename = $"{chrono} - CsProjInspector Output.xlsx";
+                outputPath = Path.Combine(applicationFolder, xlsFilename);
+            }
+
+            Console.WriteLine($"Searching csproj files in folder '{csprojPath}'");
+            ExcelHelper.GenerateExcelFile(csprojPath, outputPath);
+            Console.WriteLine("Inspection completed");
+            Console.WriteLine($"Output file generated in '{outputPath}'");
+            Console.WriteLine("Press Enter key to quit...");
+            Console.ReadLine();
+        }
+
+        static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("Usage: CsProjInspector.ConsoleApp [OPTIONS]");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
         }
     }
 }
